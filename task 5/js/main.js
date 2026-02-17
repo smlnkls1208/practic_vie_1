73645eae5e3084103331c12814c03b53527f0872
smlnkls1208 <al89994950059@gmail.com>
2026-02-17 12:38:53 +0700

    method addtocard

---
+++ task 5/js/main.js
@@ -0,0 +1,29 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        }
+    }
+
+})
+
+
