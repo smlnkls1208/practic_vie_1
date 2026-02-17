27da58fd10fc1f936e61ec66ff542572cdd1a938
smlnkls1208 <al89994950059@gmail.com>
2026-02-17 14:19:12 +0700

    вычисляемое значение title

---
+++ task 7/js/main.js
@@ -0,0 +1,39 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        brand: 'Vue Mastery',
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: false,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            }
+        ],
+        cart: 0,
+
+
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        }
+    }
+})
